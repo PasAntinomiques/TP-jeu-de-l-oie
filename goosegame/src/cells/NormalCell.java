459c27239f4ad4c2a22c52ed8f6db70ae2caf4b4
JulesM <julesmabon@gmail.com>
2018-10-17 16:24:04 +0200

    Fixed leaving null Cell

--- goosegame/src/cells/NormalCell.java
+++ goosegame/src/cells/NormalCell.java
@@ -34,8 +34,8 @@ public class NormalCell implements Cell {
 	public void welcomePlayer(Player newPlayer) {
 		Player currentPlayer = this.player;
 		Cell prevCell = newPlayer.getCell();
-		
-		prevCell.leave(newPlayer);
+		if(prevCell != null)
+			prevCell.leave(newPlayer);
 		if(currentPlayer != null) {
 			this.leave(currentPlayer);
 			prevCell.welcomePlayer(currentPlayer);
